63e370ca4e4b5423872b3bff80079468ec5378b9
pavel_milanov <pavel_milanov@iMac-pavel-milanov.local>
2022-12-25 23:01:35 +0300

    add dev tools

--- wdeditor/routers/web/web.py
+++ wdeditor/routers/web/web.py
@@ -1,8 +1,9 @@
+from typing import List
 from fastapi import APIRouter, UploadFile, status
 from fastapi.responses import FileResponse
 from services.fileeditor import FileEditor
 from services.database import db
-from models.models import TemplateContext
+from models.models import TemplateContext, Template
 
 
 router = APIRouter(
@@ -12,7 +13,15 @@ router = APIRouter(
 
 
 @router.post("/upload/")
-async def upload_template_file(file: UploadFile) -> status:   
+async def upload_template_file(file: UploadFile) -> status:
+    """Загрузка файла на сервер.
+
+    Args:
+        file (UploadFile): fastapi.UploadFile (файл пользователя).
+
+    Returns:
+        status: успешно - 201, файл уже существует - 402.
+    """      
     file, url = await FileEditor.upload_file(filename=file.filename, content=file.file)
     if file and url:
         await db.insert_template(file, url)
@@ -20,7 +29,15 @@ async def upload_template_file(file: UploadFile) -> status:
     return status.HTTP_402_PAYMENT_REQUIRED
     
 @router.post('/download/{id}')
-async def download_template(id: int):
+async def download_template(id: int)-> FileResponse:
+    """Выгрузка файла пользователю.
+
+    Args:
+        id (int): id schemas.postgres.Template.
+
+    Returns:
+        FileResponse: fastapi.responses.FileResponce.
+    """    
     model = await db.get_template(id)
     return FileResponse(
         path=model.url,
@@ -29,11 +46,24 @@ async def download_template(id: int):
     )
 
 @router.get('/')
-async def get_templates():
+async def get_templates() -> List[Template]:
+    """Возвращает все шаблоны.
+
+    Returns:
+        List[Template]: schemas.postgres.Template.
+    """
     return await db.get_templates()
 
 @router.get('/{id}')
-async def get_template(id: int):
+async def get_template(id: int) -> TemplateContext:
+    """Возвращает шаблон с тегами, которые изменяются.
+
+    Args:
+        id (int): id schemas.postgres.Template.
+
+    Returns:
+        TemplateContext: models.model.TemplateContext.
+    """
     model = await db.get_template(id)
     context = await FileEditor.get_context_file(model.url)
     return TemplateContext(
