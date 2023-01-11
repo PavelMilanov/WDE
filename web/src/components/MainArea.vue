1b6b361a7cf3749715fcc0402c9e15e22bf18c36
Pavel Milanov <milanov71@icloud.com>
2023-01-11 17:37:03 +0300

    work in web template

---
+++ web/src/components/MainArea.vue
@@ -0,0 +1,22 @@
+<script>
+
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="p-2 flex-grow-1">
+        <p>main area</p>
+    </div>
+</template>
+
+<style scoped>
+div {
+    min-height: 100%;
+}
+</style>
---
+++ web/src/components/VerticalMenu.vue
@@ -0,0 +1,23 @@
+<script>
+
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="p-2">
+      <p>vertiacal menu</p>
+    </div>
+</template>
+
+<style scoped>
+
+div {
+  background-color: bisque;
+}
+</style>
