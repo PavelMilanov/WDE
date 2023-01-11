e0c127ab836bdc379160885e56fb3b86a003a930
Pavel Milanov <milanov71@icloud.com>
2023-01-11 15:04:36 +0300

    add vue3

---
+++ web/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
